9ff1d39795b8d461220e333251e7b5f5b0f213ec
Paul <134265547+pamellix@users.noreply.github.com>
2025-12-15 21:41:28 +0300

    Выполнение задачи Таганкиным Павлом Игоревичем (#1)
    * Выполнение задачи Таганкиным Павлом Игоревичем
    
    # Conflicts:
    #	README.md
    #	src/main/java/Calculator.java
    
    * Поправил изменения
    
    ---------
    
    Co-authored-by: Таганкин Павел Игоревич <pitagankin@sberdevices.ru>

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,26 @@
+public class Calculator {
+    public int add(int a, int b){
+        return a + b;
+    }
+    public int dif(int a, int b){
+        return a - b;
+    }
+    public int div(int a, int b){
+        if (b == 0) {
+            throw new Exception("Делить на ноль нельзя!")
+        }
+        return a / b;
+    }
+    public int times(int a, int b){
+        return a * b;
+    }
+    public int solver(int a){
+        int b = 8;
+
+        int result = dif(a, b);
+        int k = div(b, result);
+
+        return k;
+
+    }
+}
